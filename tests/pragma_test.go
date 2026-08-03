4538e56d0cd85972c2a370bcbf696254bd25ff43
Angus-Warman <anguswarman@gmail.com>
2026-08-03 17:58:18 +0800

    works

--- tests/pragma_test.go
+++ tests/pragma_test.go
@@ -1,9 +1,21 @@
 package tests
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestPragmaRoundtrip(t *testing.T) {
 	db := openDB(t)
 	mustExec(t, db, "PRAGMA token -1234")
 	mustQueryColumn(t, db, "PRAGMA token", 0, []any{int64(-1234)})
+}
+
+func TestPragmaFileRoundtrip(t *testing.T) {
+	db1, fp := openFileDB(t)
+	mustExec(t, db1, "PRAGMA token -1234")
+	require.NoError(t, db1.Close())
+	db2 := reopenFileDB(t, fp)
+	mustQueryColumn(t, db2, "PRAGMA token", 0, []any{int64(-1234)})
 }
